56f12f1759d6650a1a4f50cd6aae5a3cce5a90fd
gunnmgirl <48892214+gunnmgirl@users.noreply.github.com>
2020-09-14 00:40:05 +0200

    Updates axios instance

--- src/http/index.js
+++ src/http/index.js
@@ -5,7 +5,7 @@ import { logout } from "../features/auth/actions/authActions";
 import store from "../state/store";
 
 const instance = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: process.env.REACT_APP_BASE_URL,
 });
 
 instance.interceptors.request.use(
